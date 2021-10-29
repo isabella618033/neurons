3363404721f09c20ac657f6f003c97e85ba6147e
Eugene <etesting007@gmail.com>
2021-10-29 11:35:14 -0700

    update text

--- neurons/__init__.py
+++ neurons/__init__.py
@@ -9,4 +9,7 @@ __all_neurons__ =  { 'text_template_miner': template_miner.neuron,
                      'text_template_validator': template_validator.neuron,
                      'text_template_server':template_server.neuron,
                      'text_advanced_server':advanced_server.neuron,}
-__text_neurons__ =  { 'text_template': template_miner.neuron }
+__text_neurons__ =  { 'text_template_miner': template_miner.neuron, 
+                     'text_template_validator': template_validator.neuron,
+                     'text_template_server':template_server.neuron,
+                     'text_advanced_server':advanced_server.neuron,}
